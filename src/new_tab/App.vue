5cc2dfc02a7367834fe9eaef454b37005a50134d
charlene1 <charlene.tormes@gmail.com>
2023-02-16 20:50:58 +0800

    Done with the login/logout and  fetch background photo functions

--- src/new_tab/App.vue
+++ src/new_tab/App.vue
@@ -1,42 +1,36 @@
 <template>
-    <div class="relative bg-white overflow-hidden w-screen h-screen">
-        <main class="w-full h-full flex flex-col justify-center items-center">
-            <div class="sm:text-center lg:text-left">
-                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                    <span class="block xl:inline">Vue3 + Vite</span>
-                    {{ ' ' }}
-                    <span class="block text-indigo-600 xl:inline">CE Template</span>
-                </h1>
-                <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                    occaecat fugiat aliqua.
-                </p>
-                <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                    <div class="rounded-md shadow">
-                        <a
-                            href="#"
-                            class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                        >
-                            Get started
-                        </a>
-                    </div>
-                    <div class="mt-3 sm:mt-0 sm:ml-3">
-                        <a
-                            href="#"
-                            class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                        >
-                            Live demo
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </main>
+    <div class="relative bg-white overflow-hidden w-screen h-screen" :style="root_style">
+        <Loader v-if="isLoading" />
+        <div v-else class="video-bg__wrapper" :style="root_style"></div>
+        <div class="w-full h-full relative">
+            <span
+                v-if="!isLoggedIn"
+                class="text-white absolute top-0 right-0 py-2 px-5 text-lg cursor-pointer hover:text-blue-500"
+                @click="login"
+                >Login</span
+            >
+            <span v-else class="text-white absolute top-0 right-0 py-2 px-5 text-lg cursor-pointer hover:text-blue-500" @click="logout"
+                >Logout</span
+            >
+        </div>
     </div>
 </template>
 
 <script setup>
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
 import { MenuIcon, XIcon } from '@heroicons/vue/outline'
+import Loader from './Loader.vue'
+import { onMounted, ref } from 'vue'
+import dayjs from 'dayjs'
+
+const isLoading = ref(false)
+const isLoggedIn = ref(false)
+const root_style = ref({
+    backgroundImage: ``,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundColor: 'black',
+})
 
 const navigation = [
     { name: 'Product', href: '#' },
@@ -44,4 +38,142 @@ const navigation = [
     { name: 'Marketplace', href: '#' },
     { name: 'Company', href: '#' },
 ]
+
+onMounted(async () => {
+    try {
+        const storage = await chrome.storage.local.get(['today_bg', 'isUserLoggedIn'])
+
+        console.log('storage:', storage)
+
+        const now = dayjs().format('YYYY MMMM DD')
+
+        if (!storage?.isUserLoggedIn) {
+            isLoggedIn.value = false
+            return
+        }
+
+        isLoggedIn.value = true
+
+        if ((!storage?.today_bg?.photoUrl && !storage?.today_bg?.date) || storage.today_bg.date !== now) {
+            const photoUrl = await login()
+
+            await chrome.storage.local.set({
+                today_bg: {
+                    photoUrl: photoUrl,
+                    date: now,
+                },
+                isUserLoggedIn: true,
+            })
+        } else {
+            root_style.value = {
+                backgroundImage: `url(${storage?.today_bg?.photoUrl})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                backgroundColor: 'black',
+            }
+        }
+    } catch (e) {
+        console.log('not logged in yet')
+        isLoggedIn.value = false
+    }
+})
+
+const login = async () => {
+    try {
+        isLoading.value = true
+        const token = await oauth2()
+        if (token) {
+            const photoUrl = await fetchPhoto(token)
+            const now = dayjs().format('YYYY MMMM DD')
+
+            await chrome.storage.local.set({
+                today_bg: {
+                    photoUrl: photoUrl,
+                    date: now,
+                },
+                isUserLoggedIn: true,
+            })
+
+            isLoggedIn.value = true
+
+            return photoUrl
+        }
+
+        return null
+    } catch (e) {
+        console.log('here')
+        isLoggedIn.value = false
+
+        return null
+    } finally {
+        isLoading.value = false
+    }
+}
+
+const logout = async () => {
+    chrome.identity.launchWebAuthFlow({ url: 'https://accounts.google.com/logout' }, async function(tokenUrl) {
+        isLoggedIn.value = false
+        root_style.value = {
+            backgroundImage: ``,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundColor: 'black',
+        }
+
+        await chrome.storage.local.set({
+            isUserLoggedIn: false,
+        })
+    })
+}
+
+const fetchPhoto = async (token) => {
+    let init = {
+        method: 'GET',
+        async: true,
+        headers: {
+            Authorization: 'Bearer ' + token,
+            'Content-Type': 'application/json',
+        },
+        contentType: 'json',
+    }
+
+    const API_KEY = 'AIzaSyCdljn7WuW-fwKQ6NLyXfalIrZ51wEhgFs'
+    const GOOGLE_PHOTOS_CLIENT_ID = '878246093837-d02f95nsmt62qprj56fva6anok83dqn7.apps.googleusercontent.com'
+    const data = await fetch(`https://photoslibrary.googleapis.com/v1/mediaItems`, init).then((response) => {
+        return response.json()
+    })
+
+    const photos = data?.mediaItems ?? []
+    const randomInt = Math.random() * (photos.length - 1 - 0) + 0
+    const floor = Math.floor(randomInt)
+
+    const photoUrl = photos[floor]?.baseUrl
+    const width = photos[floor]?.mediaMetadata?.width ?? '1280'
+    const height = photos[floor]?.mediaMetadata?.height ?? '720'
+
+    const url = `${photoUrl}=w${width}-h${height}`
+    const blob = await fetch(url)
+
+    root_style.value = {
+        backgroundImage: `url(${url})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundColor: 'black',
+    }
+
+    return url
+}
+
+const oauth2 = () => {
+    return new Promise((resolve) => {
+        chrome.identity.getAuthToken({ interactive: true }, function(token) {
+            console.log(token)
+            resolve(token)
+        })
+    })
+}
 </script>
+<style scoped>
+@import '../css/bundle.css';
+@import '../css/style.css';
+</style>
